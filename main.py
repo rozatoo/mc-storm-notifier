5df7903e5c8cb7f31e5568701ac58134e5f64157
alexx2021 <78677978+alexx2021@users.noreply.github.com>
2022-07-07 13:00:05 -0700

    remove unused db code

--- main.py
+++ main.py
@@ -23,9 +23,10 @@ bot = commands.Bot(
     allowed_mentions=discord.AllowedMentions(roles=False, users=True, everyone=True),
     )
 
+#We dont need a db at this time
 
-loop = asyncio.get_event_loop()
-bot.db = loop.run_until_complete(aiosqlite.connect('database.db'))
+##loop = asyncio.get_event_loop()
+##bot.db = loop.run_until_complete(aiosqlite.connect('database.db'))
 bot.currentlyThundering = False
 bot.doScrape = True
 bot.errorCH = 994483840087248906
@@ -49,7 +50,7 @@ async def on_ready():
 
 async def main():
     #Setup the DB if it hasn't been done already
-    await utils.dbsetup.setup(bot)
+    ##await utils.dbsetup.setup(bot)
     #Load extensions
     for ext in extensions:
         await bot.load_extension(f"cogs.{ext}")
