5f1bb50ffd0d4dc28227bdeb8e1420ef1c88809e
alexx2021 <78677978+alexx2021@users.noreply.github.com>
2022-07-06 23:51:40 -0700

    ctx user does not exist

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,22 +1,21 @@
 from discord.ext import commands
-from arsenic import browsers, services
-import arsenic
 
 class Commands(commands.Cog, command_attrs=dict(hidden=False)):
     def __init__(self, bot):
         self.bot = bot 
 
+    @commands.guild_only()
     @commands.command()
     async def disable(self, ctx):
-        if ctx.user.id != 982849405357019166:
+        if ctx.author.id != 982849405357019166:
             return await ctx.reply("no")
         else:
             self.bot.doScrape = False
             return await ctx.reply("ok! disabled")
-    
+    @commands.guild_only()
     @commands.command()
     async def enable(self, ctx):
-        if ctx.user.id != 982849405357019166:
+        if ctx.author.id != 982849405357019166:
             return await ctx.reply("no")
         else:
             self.bot.doScrape = True
